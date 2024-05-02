b28abac388dbaeff8ab8cd6939e10620067cd5a5
skandritgit <110586181+skandritgit@users.noreply.github.com>
2024-05-03 02:26:06 +0530

    add cart page,auth

--- server.js
+++ server.js
@@ -6,13 +6,7 @@ const expresslayouts=require("express-ejs-layouts");
 
 
 const app=express();
-
-app.get("/",(req,res)=>{
-    res.render("home");
-})
-
-app.use(express.static('public'));
-
+app.use(expresslayouts);
 app.set('view engine', "ejs");
 app.set('views',path.join(__dirname,"/resources/views"));
 
@@ -20,7 +14,24 @@ app.set('views',path.join(__dirname,"/resources/views"));
 
 
 
-app.use(expresslayouts);
+app.use(express.static('public'));
+
+app.get("/login",(req,res)=>{
+    res.render("auth/login")
+})
+app.get("/register",(req,res)=>{
+    res.render("auth/register")
+})
+
+app.get("/",(req,res)=>{
+    res.render("home");
+})
+app.get("/cart",(req,res)=>{
+    res.render("customers/cart");
+})
+
+
+
 app.listen(port,()=>{
     console.log(`Server is connected at ${port}`);
 })
